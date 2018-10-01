8a53a18177a8fd83d6c208711f4f04c077298f0a
谢海伦 <xiehailun@u51.com>
2018-10-01 20:31:06 +0800

    init project

---
+++ src/main/java/com/xhldtc/App.java
@@ -0,0 +1,13 @@
+package com.xhldtc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
