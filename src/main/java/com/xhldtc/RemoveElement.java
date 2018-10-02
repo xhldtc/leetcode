f7d73b3a16950d6ac60de9f8dc94f7c19f132f85
xhldtc <xiehailun@u51.com>
2018-10-02 19:11:02 +0800

    some problems

---
+++ src/main/java/com/xhldtc/RemoveElement.java
@@ -0,0 +1,17 @@
+package com.xhldtc;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int sum = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == val) {
+                sum++;
+                for (int j = i + 1; j < nums.length; j++) {
+                    nums[j - 1] = nums[j];
+                }
+            }
+        }
+        return nums.length - sum;
+    }
+}
