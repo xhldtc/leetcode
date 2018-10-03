56567c096152e09989da5ffacc6f948f01c23fd2
xhldtc <xiehailun@u51.com>
2018-10-03 11:16:54 +0800

    some problems

---
+++ src/main/java/com/xhldtc/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package com.xhldtc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+
+    private Map<Integer, Integer> map = new HashMap<>();
+
+    public int climbStairs(int n) {
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        if (n == 1) {
+            map.put(1, 1);
+            return 1;
+        } else if (n == 2) {
+            map.put(2, 2);
+            return 2;
+        } else {
+            int result = climbStairs(n - 1) + climbStairs(n - 2);
+            map.put(n, result);
+            return result;
+        }
+    }
+}
---
+++ src/main/java/com/xhldtc/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+package com.xhldtc;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            if (prev != null && curr.val == prev.val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
