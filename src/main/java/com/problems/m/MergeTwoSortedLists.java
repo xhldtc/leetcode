b74ce8d24f705d1a9a6785f8cc6eaed0bcb99f04
xhldtc <xiehailun@u51.com>
2019-01-21 22:44:31 +0800

    refactor

--- src/main/java/com/problems/m/MergeTwoSortedLists.java
+++ src/main/java/com/problems/m/MergeTwoSortedLists.java
@@ -5,55 +5,30 @@ import com.structure.ListNode;
 public class MergeTwoSortedLists {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result = null;
-        ListNode current = null;
-        if (l1 == null && l2 == null) {
-            return null;
-        } else if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        } else {
-            while (l1 != null && l2 != null) {
-                if (l1.val < l2.val) {
-                    if (current == null) {
-                        current = new ListNode(l1.val);
-                        result = current;
-                    } else {
-                        current.next = new ListNode(l1.val);
-                        current = current.next;
-                    }
-                    l1 = l1.next;
-                } else if (l2.val < l1.val) {
-                    if (current == null) {
-                        current = new ListNode(l2.val);
-                        result = current;
-                    } else {
-                        current.next = new ListNode(l2.val);
-                        current = current.next;
-                    }
-                    l2 = l2.next;
-                } else {
-                    if (current == null) {
-                        current = new ListNode(l1.val);
-                        current.next = new ListNode(l2.val);
-                        result = current;
-                        current = current.next;
-                    } else {
-                        current.next = new ListNode(l1.val);
-                        current.next.next = new ListNode(l2.val);
-                        current = current.next.next;
-                    }
-                    l1 = l1.next;
-                    l2 = l2.next;
-                }
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
             }
-            if (l1 != null) {
-                current.next = l1;
-            } else if (l2 != null) {
-                current.next = l2;
-            }
-            return result;
+            curr = curr.next;
         }
+        curr.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode result = new MergeTwoSortedLists().mergeTwoLists(l1, l2);
+        System.out.println(result);
     }
 }
