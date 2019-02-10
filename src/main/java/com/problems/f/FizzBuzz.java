74fbd057349b3d22fe454e59cdfa966b00a7c630
xhldtc <xiehailun@u51.com>
2019-02-10 21:40:29 +0800

    problem

---
+++ src/main/java/com/problems/f/FizzBuzz.java
@@ -0,0 +1,26 @@
+package com.problems.f;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xhldtc on 2019-02-10.
+ */
+public class FizzBuzz {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
