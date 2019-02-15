636f8dc1d3b88af1a74c779c184eb8ebff9296d2
xhldtc <xiehailun@u51.com>
2019-02-15 23:31:00 +0800

    problem

---
+++ src/main/java/com/problems/r/RotateImage.java
@@ -0,0 +1,40 @@
+package com.problems.r;
+
+/**
+ * @author xhldtc on 2019-02-15.
+ */
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = i; j < len - i - 1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - 1 - j][i];
+                matrix[len - 1 - j][i] = matrix[len - 1 - i][len - 1 - j];
+                matrix[len - 1 - i][len - 1 - j] = matrix[j][len - 1 - i];
+                matrix[j][len - 1 - i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}
+        };
+        new RotateImage().rotate(matrix);
+        print(matrix);
+    }
+
+    private static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
